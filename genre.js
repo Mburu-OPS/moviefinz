1f52428d087ebbbc3b78bc80043887cfd5174a61
Marios Daskalas <25797700+mariosdaskalas@users.noreply.github.com>
2022-04-15 19:07:39 +0300

    Add images

--- genre.js
+++ genre.js
@@ -7,48 +7,127 @@ const genre =
 async function getApi(url) {
   const response = await fetch(url);
   let data = await response.json();
+  let arrImage = [
+    "./images/action.jpg",
+    "./images/adventure.jpg",
+    "./images/animation.jpg",
+    "./images/comedy.jpg",
+    "./images/crime.jpg",
+    "./images/documentary.jpg",
+    "./images/drama.jpg",
+    "./images/family.jpg",
+    "./images/fantasy.jpg",
+    "./images/history.jpg",
+    "./images/horror.png",
+    "./images/music.jpg",
+    "./images/mystery.jpg",
+    "./images/romance.jpg",
+    "./images/science-fiction.jpg",
+    "./images/tv.jpg",
+    "./images/thriller.jpg",
+    "./images/war.jpg",
+    "./images/western.jpg",
+  ];
   for (let i = 0; i < data.genres.length; i++) {
     let obj = data.genres[i];
     console.log("Name: " + obj.name + " -- ID: " + obj.id);
 
-    // Genres row
-    const row = document.createElement("div");
-    row.classList.add("row", "m-3");
+    if (i == 0) {
+      // Genres row
+      const row = document.createElement("div");
+      row.classList.add("row", "m-3");
 
-    // Genres col-4
-    const col4 = document.createElement("div");
-    col4.classList.add("col-sm-4");
+      // Genres col-4
+      const col4 = document.createElement("div");
+      col4.classList.add("col-sm-4");
 
-    // Genres Card
-    const genresCard = document.createElement("div");
-    genresCard.classList.add("card");
+      // Genres Card
+      const genresCard = document.createElement("div");
+      genresCard.classList.add("card");
 
-    // Genres Header
-    const genresHeader = document.createElement("div");
-    genresHeader.classList.add("card-header", "text-center");
-    genresHeader.innerHTML = `${obj.name}`;
+      // Genres Header
+      const genresHeader = document.createElement("div");
+      genresHeader.classList.add("card-header", "text-center");
+      genresHeader.innerHTML = `${obj.name}`;
+      genresHeader.style.backgroundColor = "#5d5fb9";
 
-    // Card Body
-    const genresBody = document.createElement("div");
-    genresBody.classList.add("card-body");
+      // Card Body
+      const genresBody = document.createElement("div");
+      genresBody.classList.add("card-body");
 
-    // Blockquote
-    const genresBlockquote = document.createElement("blockquote");
-    genresBlockquote.classList.add("blockquote", "mb-0");
+      // Blockquote
+      const genresBlockquote = document.createElement("blockquote");
+      genresBlockquote.classList.add("blockquote", "mb-0", "text-center");
 
-    // Card paragraph
-    const genresPara = document.createElement("p");
-    genresPara.classList.add("text-center");
-    genresPara.innerHTML = `ID: ${obj.id}`;
+      // Card paragraph
+      const genresPara = document.createElement("p");
+      genresPara.classList.add("text-center");
+      genresPara.innerHTML = `ID: ${obj.id}`;
 
-    // Append Child
-    row.appendChild(col4);
-    col4.appendChild(genresCard);
-    genresCard.appendChild(genresHeader);
-    genresCard.appendChild(genresBody);
-    genresBody.appendChild(genresBlockquote);
-    genresBlockquote.appendChild(genresPara);
-    document.body.appendChild(row);
+      // Card Image
+      const genresImage = document.createElement("img");
+      genresImage.src = arrImage[i];
+      genresImage.classList.add("img", "img-fluid", "rounded");
+      genresImage.style.width = "250px";
+      genresImage.style.height = "auto";
+
+      // Append Child
+      row.appendChild(col4);
+      col4.appendChild(genresCard);
+      genresCard.appendChild(genresHeader);
+      genresCard.appendChild(genresBody);
+      genresBody.appendChild(genresBlockquote);
+      genresBlockquote.appendChild(genresPara);
+      genresBlockquote.appendChild(genresImage);
+      document.body.appendChild(row);
+    } else {
+      // Genres row
+      const row = document.createElement("div");
+      row.classList.add("row", "m-3");
+
+      // Genres col-4
+      const col4 = document.createElement("div");
+      col4.classList.add("col-sm-4");
+
+      // Genres Card
+      const genresCard = document.createElement("div");
+      genresCard.classList.add("card");
+
+      // Genres Header
+      const genresHeader = document.createElement("div");
+      genresHeader.classList.add("card-header", "text-center");
+      genresHeader.innerHTML = `${obj.name}`;
+      genresHeader.style.backgroundColor = "#5d5fb9";
+
+      // Card Body
+      const genresBody = document.createElement("div");
+      genresBody.classList.add("card-body");
+
+      // Blockquote
+      const genresBlockquote = document.createElement("blockquote");
+      genresBlockquote.classList.add("blockquote", "mb-0", "text-center");
+
+      // Card paragraph
+      const genresPara = document.createElement("p");
+      genresPara.classList.add("text-center");
+      genresPara.innerHTML = `ID: ${obj.id}`;
+
+      // Card Image
+      const genresImage = document.createElement("img");
+      genresImage.src = arrImage[i];
+      genresImage.classList.add("img", "img-fluid", "rounded");
+      genresImage.style.width = "250px";
+      genresImage.style.height = "auto";
+
+      // Append Child
+      col4.appendChild(genresCard);
+      genresCard.appendChild(genresHeader);
+      genresCard.appendChild(genresBody);
+      genresBody.appendChild(genresBlockquote);
+      genresBlockquote.appendChild(genresPara);
+      genresBlockquote.appendChild(genresImage);
+      document.body.appendChild(col4);
+    }
   }
 }
 
