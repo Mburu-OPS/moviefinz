3f64977575836b8ccd04d7c9dc24ffa39fc60c79
Marios Daskalas <25797700+mariosdaskalas@users.noreply.github.com>
2022-04-16 13:03:54 +0300

    Fetch action search movies

--- genre.js
+++ genre.js
@@ -33,61 +33,61 @@ async function getApi(url) {
     let obj = data.genres[i];
     console.log("Name: " + obj.name + " -- ID: " + obj.id);
 
-    if (i == 0) {
-      // Genres row
-      const row = document.createElement("div");
-      row.classList.add("row", "m-3");
+    // Genres row
+    const row = document.createElement("div");
+    row.classList.add("row", "m-3");
 
-      // Genres col-4
-      const col4 = document.createElement("div");
-      col4.classList.add("col-sm-4");
+    // Genres col-4
+    const col4 = document.createElement("div");
+    col4.classList.add("col-sm-4");
 
-      // Genres Card
-      const genresCard = document.createElement("div");
-      genresCard.classList.add("card");
+    // Genres Card
+    const genresCard = document.createElement("div");
+    genresCard.classList.add("card");
 
-      // Genres Header
-      const genresHeader = document.createElement("div");
-      genresHeader.classList.add("card-header", "text-center");
-      genresHeader.innerHTML = `${obj.name}`;
-      genresHeader.style.backgroundColor = "#5d5fb9";
+    // Genres Header
+    const genresHeader = document.createElement("div");
+    genresHeader.classList.add("card-header", "text-center");
+    genresHeader.innerHTML = `${obj.name}`;
+    genresHeader.style.backgroundColor = "#5d5fb9";
 
-      // Card Body
-      const genresBody = document.createElement("div");
-      genresBody.classList.add("card-body");
+    // Card Body
+    const genresBody = document.createElement("div");
+    genresBody.classList.add("card-body");
 
-      // Blockquote
-      const genresBlockquote = document.createElement("blockquote");
-      genresBlockquote.classList.add("blockquote", "mb-0", "text-center");
+    // Blockquote
+    const genresBlockquote = document.createElement("blockquote");
+    genresBlockquote.classList.add("blockquote", "mb-0", "text-center");
 
-      // Card paragraph
-      const genresPara = document.createElement("p");
-      genresPara.classList.add("text-center");
-      genresPara.innerHTML = `ID: ${obj.id}`;
+    // Card paragraph
+    const genresPara = document.createElement("p");
+    genresPara.classList.add("text-center");
+    genresPara.innerHTML = `ID: ${obj.id}`;
 
-      // Card Link
-      const genresLink = document.createElement("a");
-      genresLink.href = arrLink[i];
-      genresLink.innerHTML = "Search";
+    // Card Link
+    const genresLink = document.createElement("a");
+    genresLink.href = arrLink[i];
+    genresLink.innerHTML = "Search";
 
-      // Card Image
-      const genresImage = document.createElement("img");
-      genresImage.src = arrImage[i];
-      genresImage.classList.add("img", "img-fluid", "rounded");
-      genresImage.style.width = "250px";
-      genresImage.style.height = "auto";
+    // Card Image
+    const genresImage = document.createElement("img");
+    genresImage.src = arrImage[i];
+    genresImage.classList.add("img", "img-fluid", "rounded");
+    genresImage.style.width = "250px";
+    genresImage.style.height = "auto";
 
-      // Append Child
-      row.appendChild(col4);
-      col4.appendChild(genresCard);
-      genresCard.appendChild(genresHeader);
-      genresCard.appendChild(genresBody);
-      genresBody.appendChild(genresBlockquote);
-      genresBlockquote.appendChild(genresPara);
-      genresBlockquote.appendChild(genresImage);
-      genresBlockquote.appendChild(genresLink);
-      document.body.appendChild(row);
-    } else {
+    // Append Child
+    row.appendChild(col4);
+    col4.appendChild(genresCard);
+    genresCard.appendChild(genresHeader);
+    genresCard.appendChild(genresBody);
+    genresBody.appendChild(genresBlockquote);
+    genresBlockquote.appendChild(genresPara);
+    genresBlockquote.appendChild(genresImage);
+    genresBlockquote.appendChild(genresLink);
+    document.body.appendChild(row);
+  } /*
+    else {
       // Genres row
       const row = document.createElement("div");
       row.classList.add("row", "m-3");
@@ -141,7 +141,7 @@ async function getApi(url) {
       genresBlockquote.appendChild(genresLink);
       document.body.appendChild(col4);
     }
-  }
+    */
 }
 
 getApi(genre);
